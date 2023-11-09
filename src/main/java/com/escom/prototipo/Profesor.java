93300935feddbcdce8033e01a83281ced070069f
DamarisServin <30834629+DamarisServin@users.noreply.github.com>
2023-11-08 23:10:59 -0600

    Actualizar Profesor.java

---
+++ src/main/java/com/escom/prototipo/Profesor.java
@@ -0,0 +1,16 @@
+
+public class Profesor{
+ 
+    String nombre;
+    String contrasena;
+    
+    public void setNombre(String nombre){
+        this.nombre =nombre;
+    }
+
+    public void setContrasena(String contrasena){
+        this.contrasena = contrasena;
+    }
+    
+
+}
