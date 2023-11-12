062d319073e8de84ed74def654ad04fdb6f6550d
DamarisServin <30834629+DamarisServin@users.noreply.github.com>
2023-11-12 17:41:54 -0600

    Actualizar Profesor.java

--- src/main/java/com/escom/prototipo/Profesor.java
+++ src/main/java/com/escom/prototipo/Profesor.java
@@ -1,23 +1,46 @@
 
 public class Profesor{
  
-    String nombre;
+    String nombre_completo;
     String contrasena;
+    String correo;
+    String escuela;
+    String no_poli;
+
     
-    public void setNombre(String nombre){
-        this.nombre =nombre;
+    public void setNombreCompleto(String nombre, String ap_paterno, String ap_materno){
+        this.nombre_completo =nombre+ap_paterno+ap_materno;
+    }
+    public void setCorreo(String correo){
+        this.correo = correo;
     }
 
     public void setContrasena(String contrasena){
         this.contrasena = contrasena;
     }
 
+    public void setEscuela(String escuela){
+        this.escuela = escuela;
+    }
+    
+    public void setNoPoli(String no_poli){
+        this.no_poli = no_poli;
+    }
+
     public String getNombre(){
-        return this.nombre;
+        return this.nombre_completo;
     }
 
     public String getContrasena(){
         return this.contrasena;
+    }
+
+    public String getCorreo(){
+        return this.correo;
+    }
+    
+    public String getNoPoli(){
+        return this.no_poli;
     }
 
 
