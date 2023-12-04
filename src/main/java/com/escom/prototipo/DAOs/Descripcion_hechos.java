d64fc31ea2296a28e82d85a04eca64452ead4f86
DamarisServin <dservinq1500@alumno.ipn.mx>
2023-12-04 03:33:29 -0600

    Crea objeto denuncia

--- src/main/java/com/escom/prototipo/DAOs/Descripcion_hechos.java
+++ src/main/java/com/escom/prototipo/DAOs/Descripcion_hechos.java
@@ -9,5 +9,50 @@ package com.escom.prototipo.DAOs;
  * @author damar
  */
 public class Descripcion_hechos {
+    private String fecha;
+    private String lugar;
+    private String descripcion;
+    private String testigos;
+
+    public Descripcion_hechos(String fecha, String lugar, String descripcion, String testigos) {
+        this.fecha = fecha;
+        this.lugar = lugar;
+        this.descripcion = descripcion;
+        this.testigos = testigos;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTestigos() {
+        return testigos;
+    }
+
+    public void setTestigos(String testigos) {
+        this.testigos = testigos;
+    }
+    
+    
     
 }
